772681defd50b8fdb32feff54859e1992bc94acf
silicona <alejoizquierdomartinez@gmail.com>
2024-05-09 13:21:44 +0200

    unify reports

---
+++ codeception/tests/Acceptance/FirstCest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Llegamos');
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+
+</head>
+<body>
+    <h2>Llegamos</h2>
+</body>
+</html>
